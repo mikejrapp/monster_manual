0310e3c7cb303b045912947c920b56c3ffd20eaf
Mike <mikejrapp@gmail.com>
2019-01-01 17:27:23 -0700

    added additional base stats to back, started search bar section

--- src/components/ActionBlock.js
+++ src/components/ActionBlock.js
@@ -3,7 +3,10 @@ import React from "react";
 const ActionBlock = () =>{
     return(
         <div className={"action-block"}>
-
+            <div className="action-wrapper">
+                <h3 className="action-block-search-title">Search The Ancient Tomes</h3>
+                <input type="text" className="search"/>
+            </div>
         </div>
     )
 };
--- src/components/BaseStats.js
+++ src/components/BaseStats.js
@@ -6,26 +6,77 @@ const BaseStats = () =>{
 
             <div className={"base-stats-card"}>
 
-                <div className={"base-stats-card-side base-stats-card-front"}>
-                    <div className={"base-stats-heading"}>
-                    <h3 className={"base-stats-heading-header"}>Lich</h3>
-                    <p className={"base-stats-heading-subtext"}>Medium Undead, any evil alignment</p>
-                </div>
-                <div className={"base-stats-icons"}>
-                    <div className={"base-stats-icons_wrapper"}>
-                        <i className="fas fa-shield-alt fa-3x armor-class"><span className={"armor-class-span"}>17</span></i>
-                    </div>
-                    <div className={"base-stats-icons_wrapper"}>
-                        <i className="fas fa-heart fa-3x  health-points"><span className={"health-points-span"}>135</span></i>
-                    </div>
-                    <div className={"base-stats-icons_wrapper"}>
-                        <i className="fas fa-bolt fa-3x base-speed"><span className={"base-speed-span"}>30 ft</span></i>
-                    </div>
-                </div>
-                </div>
+                <div className="base-stats-card-inner">
 
-                <div className={"base-stats-card-side base-stats-card-back"}>
-                    test
+                    <div className="base-stats-card-front">
+                        <div className={"base-stats-heading"}>
+                            <h3 className={"base-stats-heading-header"}>Lich</h3>
+                            <p className={"base-stats-heading-subtext"}>Medium Undead, any evil alignment</p>
+                        </div>
+
+                        <div className={"base-stats-icons"}>
+                            <div className={"base-stats-icons_wrapper"}>
+                                <i className="fas fa-shield-alt fa-3x armor-class"><span className={"armor-class-span"}>17</span></i>
+                            </div>
+                            <div className={"base-stats-icons_wrapper"}>
+                                <i className="fas fa-heart fa-3x  health-points"><span className={"health-points-span"}>135</span></i>
+                            </div>
+                            <div className={"base-stats-icons_wrapper"}>
+                                <i className="fas fa-bolt fa-3x base-speed"><span className={"base-speed-span"}>30 ft</span></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="base-stats-card-back">
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Saving Throws</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-throws">CON +10</p>
+                            <p className="base-stats-throws">INT +12</p>
+                            <p className="base-stats-throws">WIS +9</p>
+                        </div>
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Skills</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-throws">Arcana +19</p>
+                            <p className="base-stats-throws">History +12</p>
+                            <p className="base-stats-throws">Insight +9</p>
+                            <p className="base-stats-throws">Perception +9</p>
+                        </div>
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Damage Resistances</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-resistances">Cold, Lightning, Necrotic</p>
+                        </div>
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Damage Immunities</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-resistances">Poison, Bludgeoning, Piercing, Slashing from non magical items</p>
+                        </div>
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Condition Immunities</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-resistances">Charmed, Exhaustion, Frightened, Paralyzed, Poisoned</p>
+                        </div>
+
+                        <div className="base-stats-title-banner">
+                            <h4 className="base-stats-title">Senses</h4>
+                        </div>
+                        <div className="base-stats-wrapper">
+                            <p className="base-stats-resistances">Truesight 120ft, Passive Perception 19</p>
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
